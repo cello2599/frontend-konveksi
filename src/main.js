4c4f45bf77b19c1fccadc943419e00afbb46f0b2
rafly07320 <062020107320@mhs.itats.ac.id>
2023-05-13 01:30:47 +0700

    final css
    ubah bootstrap ke tailwind

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 //import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import './assets/tailwind.css'
 
 //Vue.config.productionTip = false
 
