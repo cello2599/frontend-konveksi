016d6b0196fb9414529ee0849f83397754dd2e46
cello2599 <73047281+cello2599@users.noreply.github.com>
2023-05-17 00:19:55 +0700

    Update ShowClient.vue

--- src/views/clientside/ShowClient.vue
+++ src/views/clientside/ShowClient.vue
@@ -1,6 +1,7 @@
 <template>
+    <img src="public/putihmerah.jpg" />
     <div v-for="(produk,index) in products" :key="index" class="max-w-sm rounded overflow-hidden shadow-lg" style="padding-top: 2rem;">
-        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimgsrv2.voi.id%2Fj1O7BU5uNKkhJE_7P9sELHxI4w1W0owv-VPETQOvRPM%2Fauto%2F1200%2F675%2Fsm%2F1%2FbG9jYWw6Ly8vcHVibGlzaGVycy8yMTc3MzIvMjAyMjEwMTIxMTAzLW1vYmlsZS5jcm9wcGVkXzE2NjU2MjA3MjAuanBn.jpg&tbnid=5yOaKAJrOmREBM&vet=12ahUKEwjw_oDk2ff-AhWDKbcAHSI4D0wQMygHegUIARDPAQ..i&imgrefurl=https%3A%2F%2Fvoi.id%2Fberita%2F217732%2Faturan-seragam-sekolah-terbaru-orang-tua-tak-harus-dibebankan-seragam-khusus-sekolah&docid=75e5ZElaX65BBM&w=1200&h=675&q=Seragam%20Sekolah&ved=2ahUKEwjw_oDk2ff-AhWDKbcAHSI4D0wQMygHegUIARDPAQ" />
+        
         <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2" style="text-transform: uppercase;">{{ produk.nama_produk }}</div>
                             <!-- <h4>Rp. </h4> -->
