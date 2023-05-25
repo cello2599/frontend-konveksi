820149464248bdcf95a6438393856399788dbba9
rafly07320 <062020107320@mhs.itats.ac.id>
2023-05-26 00:14:45 +0700

    tombol edit

--- src/views/clientside/ShowClient.vue
+++ src/views/clientside/ShowClient.vue
@@ -26,9 +26,7 @@
         
         </div>
         <RouterLink :to="{name : 'EditProduk', params:{id : produk.id_produk}}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-</svg>
+            <i class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</i>
 
         </RouterLink>
     </div>
@@ -84,6 +82,7 @@ export default {
         padding-top: 10px;
         margin-left: 17rem;
         margin-bottom: 3rem;
+        margin-top: 2rem;
         display:flex;
 
     }
