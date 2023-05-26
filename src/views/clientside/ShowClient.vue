e2a82ff38b2ee976a21d6f1a4760570b6d25a935
rafly07320 <062020107320@mhs.itats.ac.id>
2023-05-26 20:45:18 +0700

    fungsi delete

--- src/views/clientside/ShowClient.vue
+++ src/views/clientside/ShowClient.vue
@@ -30,8 +30,8 @@
 
         </RouterLink>
 
-        <button @click="deleteProduk(produk.id_produk)" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
-        
+        <button @click="deleteProduk(produk.id_produk, index)" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
+
     </div>
     </div>
 </div>
@@ -49,6 +49,7 @@ export default {
         const products = ref([]);
         //get token
         const token = localStorage.getItem('access_token');
+        
         //image path
         //const imagepath = ref('src/assets/images/');
 
@@ -68,9 +69,23 @@ export default {
             }
         });
 
+        //delete produk function
+        const deleteProduk = async(id, index) => {
+            if(!confirm('Are you sure?')) return;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+            try{
+                const response = await axios.delete('http://127.0.0.1:8000/api/produk/' + id);
+                products.value.splice(index, 1);
+                console.log(response.data.message);
+            }catch(err){
+                console.log(err);
+            }
+        }
+
+
         return {
             products
-            //imagepath
+            ,deleteProduk
         }
     },
     components:{
