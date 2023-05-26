5558561be4ce74af5f8abe70037ccddee27acd57
rafly07320 <062020107320@mhs.itats.ac.id>
2023-05-26 20:32:43 +0700

    tombol delete

--- src/views/clientside/ShowClient.vue
+++ src/views/clientside/ShowClient.vue
@@ -29,6 +29,9 @@
             <i class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</i>
 
         </RouterLink>
+
+        <button @click="deleteProduk(produk.id_produk)" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
+        
     </div>
     </div>
 </div>
