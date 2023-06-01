ed4c0f1e2232766677a1ff8850caf3e45919184c
rafly07320 <062020107320@mhs.itats.ac.id>
2023-06-02 01:06:32 +0700

    Select Kategori Ukuran

--- src/views/clientside/CreateProduk.vue
+++ src/views/clientside/CreateProduk.vue
@@ -20,7 +20,8 @@
 
                             <div class="form-group">
                                 <p><label for="genre">ID Ukuran</label></p>
-                                <select v-model="id_ukuran">
+                                <select v-model="id_ukuran" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option disabled value="">Pilih Ukuran</option>
                                         <option v-for="(ukuran, index) in ukurans" :value="ukuran.id_ukuran" :key="index">
                                             {{ ukuran.id_ukuran }} - {{ ukuran.ukuran }}
                                         </option>
@@ -30,7 +31,8 @@
 
                             <div class="form-group">
                                 <p><label for="genre">ID Kategori</label></p>
-                                <select v-model="id_kategori">
+                                <select v-model="id_kategori" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option disabled value="">Pilih Kategori</option>
                                         <option v-for="(kategori, index) in kategoris" :value="kategori.id_kategori" :key="index">
                                             {{ kategori.id_kategori }} - {{ kategori.kategori_produk }}
                                         </option>
